898aa58853bfde821e4a2fddab3c042daa39201a
Ericson-Manalo <ericmana98@gmail.com>
2023-07-20 16:19:41 +0200

    made a test to see if it works by using a foreach and movies

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,8 +5,19 @@
 
 
 @section('main-content')
-@dd($movies)
+@dump($movies)
     <h1>
         Congratulation, it works!
     </h1>
+
+    <div>
+        @foreach($movies as $movie)
+            <article>
+                <img src="" alt="">
+                <h1>
+                    {{ $movie->title }}
+                </h1>
+            </article>
+        @endforeach
+    </div>
 @endsection
