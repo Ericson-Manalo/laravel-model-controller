6a69bd5a5e50b0502c0e2576a7703328a29cef08
Ericson-Manalo <ericmana98@gmail.com>
2023-07-20 16:05:32 +0200

    called all the element presented in movie

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -4,12 +4,16 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Movie;
 
 class PageController extends Controller
 {
     //
 
     public function home(){
-        return view('home');
+
+        $movies = Movie::all();
+
+        return view('home', compact('movies'));
     }
 }
